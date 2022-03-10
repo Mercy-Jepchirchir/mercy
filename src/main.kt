752fcd3feae695d1d5ed0d85f374b4ede60bda8d
Mercy-Jepchirchir <mjepchirchirkemboi@gmail.com>
2022-03-10 09:02:09 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+fun main(){
+remeinder()
+    var z= me("chirii","34")
+    println(z)
+}
+//fun campus("CodeHive"):string{
+//var Char5= [4]
+//var Char6= [5]
+//var Char7= [6]
+//var char8= [7]
+//println(campus())
+
+//}
+fun remeinder(){
+    var number1= 9
+    var number2= 2
+     println(number1%number2)
+
+
+}
+fun me(name:String,age:String) :String{
+    var abiy= ("Hi, my name is $name and i am$age years old")
+    return abiy
+}
+fun myname(){
+
+}
